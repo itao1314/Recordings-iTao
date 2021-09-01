2dd37c830dd3fe5b4dec288ac193e9dba59520a5
Tao.T <itao_1314@icloud.com>
2021-09-01 16:29:14 +0800

    初始化项目

---
+++ Recordings-iTao/Recordings-iTao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recordings-iTao
+//
+//  Created by 陶涛 on 2021/9/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
